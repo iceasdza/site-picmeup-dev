5a60320af0f19357e60cd9db9895482ee506620e
Warhead007 <rawit_ham@hotmail.com>
2018-04-11 00:10:37 +0700

    Modify Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import Header_Picmeup from '../UI/Header_Picmeup';
+import Place_Form from '../UI/Place_Form';
+
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1>
                     <Header_Picmeup/>
+                    <Place_Form/>
                 </h1>
             </div>
         )
