c451feb04abace733f159b9629fa4d17fd184946
iceasdza <ice_ro.patis@hotmail.com>
2018-08-19 13:50:33 +0700

    [edit] get date and some form

--- src/components/users/register/registerForm.js
+++ src/components/users/register/registerForm.js
@@ -1,52 +1,52 @@
-import React from 'react'
-import { Form,Card,Icon,Image } from 'semantic-ui-react'
+import React from "react";
+import { Form, Image, Button } from "semantic-ui-react";
+import DatePicker from 'react-datepicker';
 import "../../../static/Form.css";
 
 const options = [
-    { key: 'm', text: 'Male', value: 'male' },
-    { key: 'f', text: 'Female', value: 'female' },
-  ]
+  { key: "m", text: "Male", value: "male" },
+  { key: "f", text: "Female", value: "female" }
+];
 
 
-const RegisterForm = (props) =>{
-        return(
-            <div className="container fluid">
-        <Form.Group widths='equal'>
-          <Form.Input fluid label='First name' placeholder='First name' />
-          <Form.Input fluid label='Last name' placeholder='Last name' />
-          <Form.Select fluid label='Gender' options={options} placeholder='Gender' />
-        </Form.Group>
-            <br/>
-        <label className={props.isActive} onMouseEnter={props.handleOver} onMouseLeave={props.handleOut}>
-            <p className="Color">เลือกรูปภาพ</p>
-            <input
-              name="img"
-              id="img"
-              type="file"
-              style={{ display: "none" }}
-              onChange={e => props.handleSelectImage(e)}
-                          />
-          </label>
-          <Card.Group itemsPerRow={6}>
-            {props.files.map((data, index) => (
-              <Card key={index}>
-                <div>
-                  <Icon
-                    circular
-                    inverted
-                    name="remove"
-                    color="red"
-                    onClick={() => props.DeletePhotoUploaded("files", index)}
-                  />
-                </div>
-                <Image src={data} />
-              </Card>
-            ))}
-          </Card.Group>
-            </div>
-            
-        )
-    
-}
+const RegisterForm = props => {
+  return (
+    <div className="container fluid">
+      <Form.Group widths="equal">
+        <Form.Input fluid label="First name" placeholder="First name" onChange={e=>props.handleOnChange('firstName',e.target.value)}/>
+        <Form.Input fluid label="Last name" placeholder="Last name" onChange={e=>props.handleOnChange('lastName',e.target.value)}/>
+        <Form.Select
+          fluid
+          label="Gender"
+          options={options}
+          placeholder="Gender"
+          onChange={({ value }) => props.handleOnChange('gender', value)}
+        />
+        <Form.Input fluid label="email" placeholder="email" />
+      </Form.Group>
+      <br />
+      <label
+        className={props.isActive}
+        onMouseEnter={props.handleOver}
+        onMouseLeave={props.handleOut}
+      >
+        <p className="Color">เลือกรูปภาพ</p>
+        <input
+          name="img"
+          id="img"
+          type="file"
+          style={{ display: "none" }}
+          onChange={e => props.handleSelectImage(e)}
+        />
+      </label>
+      <Image src={props.files} circular className="avatar-uploaded" />
+      <DatePicker
+        selected={props.startDate}
+        onChange={e=>props.handleOnChange("dateOfBirth",e._d)}
+    />
+      <Button>Submit</Button>
+    </div>
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
--- src/containers/users/register/register.js
+++ src/containers/users/register/register.js
@@ -1,59 +1,74 @@
-import React,{Component} from 'react'
-import RegisterForm from '../../../components/users/register/registerForm'
-import Header from '../../../components/header/header'
-class Register extends Component{
+import React, { Component } from "react";
+import RegisterForm from "../../../components/users/register/registerForm";
+import Header from "../../../components/header/header";
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: "uploadButton",
+      files: [],
+      firstName:"",
+      lastName:"",
+      gender:"",
+      email:"",
+      imageAvatar:"",
+      startDate: moment(),
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state={
-            active:'uploadButton',
-            files:[]
-        }
+  handleOnChange=(field,e)=>{
+    console.log(field,e)
+  }
+  
+
+  handleOver = () => {
+    this.setState({ active: "uploadButton-active" });
+  };
+
+  handleOut = () => {
+    this.setState({ active: "uploadButton" });
+  };
+
+  handleSelectImage = event => {
+    const files = event.target.files;
+    const arr = [];
+    for (var x = 0; x < files.length; x++) {
+      arr.push(URL.createObjectURL(files[x]));
     }
-
- handleOver=()=>{
-    this.setState({active:"uploadButton-active"})
-}  
-
- handleOut=()=>{
-    this.setState({active:"uploadButton"})
-}  
-
-handleSelectImage=(event)=>{
-    const files  = event.target.files
-    const arr =[]
-    for(var x = 0; x < files.length; x++){
-        arr.push(URL.createObjectURL(files[x]))
-    }    
     this.setState({
-        files:arr
-    })
-}
+      files: arr
+    });
+  };
 
-DeletePhotoUploaded =(field,index)=>{
-    let arr = []
-    arr = this.state.files
-    arr.splice(index,1)
-    this.setState({files:arr})
-    }
+  DeletePhotoUploaded = (field, index) => {
+    let arr = [];
+    arr = this.state.files;
+    arr.splice(index, 1);
+    this.setState({ files: arr });
+  };
 
-
-    render(){
-        return(
-            <div>
-            <Header/>
-            <RegisterForm
+  //   { key: "m", text: "Male", value: "male" }
+  render() {
+    return (
+      <div>
+        <Header />
+        <form>
+          <RegisterForm
             handleOver={this.handleOver}
             handleOut={this.handleOut}
             handleSelectImage={this.handleSelectImage}
             DeletePhotoUploaded={this.DeletePhotoUploaded}
-            
             isActive={this.state.active}
             files={this.state.files}
-            />
-            </div>
-        )
-    }
+            handleOnChange={this.handleOnChange}
+            startDate={this.state.startDate}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Register
+export default Register;
