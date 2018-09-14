06334add381b63665fae1cd53aaec481318744ec
iceasdza <ice_ro.patis@hotmail.com>
2018-09-14 20:40:13 +0700

    [add]add quill to create topic

--- src/components/topic/createTopicComponent.js
+++ src/components/topic/createTopicComponent.js
@@ -1,15 +1,54 @@
 import React from "react";
-// import { Form, Dropdown } from "formsy-semantic-ui-react";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css'; 
+import '../../static/topic.css'
 const createTopicComponent = props => {
   return (
     <div className="container fluid">
-      {/* {props.paragraph} */}
-      {props.paragraph.map((data,key)=>(
-          <p key={key} onClickCapture={key=>props.handleClick(key)} id={key}>{data}</p>
-      ))}
-      <p><input type="text" onKeyPress={props.renderParagraph} value={props.text} onChange={e=>props.handleOnchange(e)}/></p>
-    </div>
+      <ReactQuill
+        value={props.text}
+        modules={createTopicComponent.modules}
+        formats={createTopicComponent.formats}
+        onChange={props.handleChange}
+      />
+      </div>
   );
 };
+
+createTopicComponent.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" }
+    ],
+    ["link", "image", "video"],
+    ["clean"]
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false
+  }
+};
+createTopicComponent.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video"
+];
 
 export default createTopicComponent;
--- src/containers/topic/createTopic.js
+++ src/containers/topic/createTopic.js
@@ -1,43 +1,24 @@
 import React,{Component} from 'react'
 import Navbar from '../../components/header/header'
 import CreateTopicComponent from '../../components/topic/createTopicComponent'
-import { Form } from 'formsy-semantic-ui-react'
 class CreateTopic extends Component{
-    state={
-        paragraph:[],
-        text:''
-    }
-
-    renderParagraph = (e) =>{
-        const paragraphs = this.state.paragraph
-        const text = this.state.text
-        if(e.key==='Enter'){
-            paragraphs.push(text)
-            
-            this.setState({paragraph:paragraphs,text:''})
-        }
-    }
-    handleOnchange = (e) =>{
-        this.setState({text:e.target.value})
-    }
-
-    handleClick = (e) =>{
-        console.log(e.target)
-    }
-
+    constructor(props) {
+        super(props)
+        this.state = { text: '' }
+      }
+    
+      handleChange = (value) =>{
+        this.setState({ text: value })
+        console.log(value)
+      }
     render(){
         return(
             <div>
             <Navbar/>
-            <Form>
             <CreateTopicComponent
-            paragraph={this.state.paragraph}
-            renderParagraph={this.renderParagraph}
-            handleOnchange={this.handleOnchange}
             text={this.state.text}
-            handleClick={this.handleClick}
+            handleChange={this.handleChange}
             />
-            </Form>
             </div>
         )
     }
