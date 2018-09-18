4337897ef4413a106eace0b266ee2c0636716194
Patipat_Bang <patipat.bangvnp@mail.kmutt.ac.th>
2018-09-18 18:39:49 +0700

    edit path

--- src/containers/chat/chatList.js
+++ src/containers/chat/chatList.js
@@ -1,11 +1,11 @@
 import React , {Component} from 'react'
-import Header from '../../components/header/header'
+import HeaderControl from '../header/headercontrol'
 import ChatComponent from '../../components/chat/chatComponent'
 class ChatList extends Component{
     render(){
         return(
             <div>
-            <Header/>
+            <HeaderControl/>
             <ChatComponent/>
             </div>
         )
