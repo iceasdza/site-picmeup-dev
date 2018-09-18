508e3bb7ae95cb3c77886298c437ff3abd17b037
Patipat_Bang <patipat.bangvnp@mail.kmutt.ac.th>
2018-09-18 18:36:03 +0700

    add new contron

---
+++ src/containers/header/headercontrol.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react';
+import Header from "../../components/header/header";
+
+class HeaderControl extends Component{
+    render(){
+        return(
+            <dir>
+                <Header/>
+            </dir>
+        )
+    }
+}
+export default HeaderControl
