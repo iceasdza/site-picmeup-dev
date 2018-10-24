161efc2b85cdc7ec7215d4f5db171fc983e34b56
iceasdza <ice_ro.patis@hotmail.com>
2018-10-24 19:10:23 +0700

    test geo

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'http://128.199.107.81:3030'
-axios.defaults.baseURL = 'http://localhost:3030'
+axios.defaults.baseURL = 'http://128.199.107.81:3030'
+// axios.defaults.baseURL = 'http://localhost:3030'
 
 
 export default axios
