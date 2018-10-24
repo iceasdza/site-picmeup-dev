f3d2c9bee0879c990312848869c93c2dc47dace9
iceasdza <ice_ro.patis@hotmail.com>
2018-10-24 18:41:57 +0700

    [add]distance calculate

--- src/containers/users/findByNear/findByNearPage.js
+++ src/containers/users/findByNear/findByNearPage.js
@@ -2,44 +2,77 @@ import React from "react";
 import { geolocated } from "react-geolocated";
 import axios from "../../../lib/axios";
 import Cookies from "js-cookie";
-import { Table } from "semantic-ui-react";
+import { Table,Button } from "semantic-ui-react";
 let user = Cookies.get("user");
 class Demo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userLat: "",
-      userLng: "",
-      userData: []
+      userGeoLocation: {},
+      userData: [],
+      userDistanceInfo:[]
     };
   }
 
-  componentDidUpdate() {
-    this.sendData();
-  }
-  componentWillMount() {
+  componentDidMount(){
     this.getData();
   }
+  // componentWillReceiveProps(){
+  //   this.sendData();
+  // }
 
   getData = async () => {
-    const resp = await axios.get("/api/getAllGeo/"+user);
+    const resp = await axios.get("/api/getAllGeo/" + user);
     this.setState({ userData: resp.data });
-
-    console.log(resp.data)
   };
 
   sendData = async () => {
-    if(this.props.coords === null){
-      return
+    if (this.props.coords === null) {
+      return;
     }
-    const resp = await axios.post("/api/updateGeolocation", {
-      lat: this.props.coords.latitude,
-      lng: this.props.coords.longitude,
+    const resp = await axios.put("/api/updateGeolocation", {
+      latitude: this.props.coords.latitude,
+      longitude: this.props.coords.longitude,
       user: user
     });
 
-    console.log(resp.data);
+    this.setState({
+      userGeoLocation: { latitude: resp.data.latitude, longitude: resp.data.longitude }
+    });
   };
+
+  getDistanceFromLatLonInKm=(lat1,lon1,lat2,lon2)=> {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d
+  }
+  
+  deg2rad=(deg)=>{
+    return deg * (Math.PI/180)
+  }
+
+  storeUserNearBy = () =>{
+    const currentUserLat = this.state.userGeoLocation.latitude
+    const currentUserLng = this.state.userGeoLocation.longitude
+
+    const arr = []
+    this.state.userData.map((data)=>{
+      const resp = this.getDistanceFromLatLonInKm(currentUserLat,currentUserLng,data.latitude,data.longitude)
+      arr.push({distance:resp,userName:data.userName})
+    })
+    this.setState({
+      userDistanceInfo:arr
+    })
+
+  }
 
   renderTable = () => {
     return (
@@ -48,27 +81,35 @@ class Demo extends React.Component {
           <Table.Row>
             <Table.HeaderCell>#</Table.HeaderCell>
             <Table.HeaderCell>User</Table.HeaderCell>
-            <Table.HeaderCell>Latitude</Table.HeaderCell>
-            <Table.HeaderCell>Longtitude</Table.HeaderCell>
+            <Table.HeaderCell>Distance</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          {this.state.userData.map((data,index)=>(
-                  <Table.Row key={index}>
-                  <Table.Cell>{index}</Table.Cell>
-                  <Table.Cell>{data.userName}</Table.Cell>
-                  <Table.Cell>{data.lat}</Table.Cell>
-                  <Table.Cell>{data.lng}</Table.Cell>
-                </Table.Row>
+          {this.state.userDistanceInfo.map((data, index) => (
+            <Table.Row key={index}>
+              <Table.Cell>{index}</Table.Cell>
+              <Table.Cell>{data.userName}</Table.Cell>
+              <Table.Cell>{data.distance}</Table.Cell>
+            </Table.Row>
           ))}
         </Table.Body>
       </Table>
     );
   };
 
-  render() {
-    return <div className="container fluid">{this.renderTable()}</div>;
+  render() 
+  {
+    return <div className="container fluid">
+    <Button onClick={this.storeUserNearBy}>
+      คำนวนสิ
+    </Button>
+    <Button onClick={this.sendData}>
+      ระบุพิกัด
+    </Button>
+  
+    {this.renderTable()}
+    </div>;
   }
 }
 
